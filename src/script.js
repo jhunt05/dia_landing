05ec6f80a559f050bda3816849444a377b03c265
Viktoriia <jhunt@ukr.net>
2019-11-04 11:00:17 +0200

    fix

--- src/script.js
+++ src/script.js
@@ -32,3 +32,35 @@ function nextPhoto() {
 }
 
 setInterval(nextPhoto, 7000);
+
+// Menu anchors
+
+let linkNav = document.querySelectorAll('[href^="#"]');
+const V = 1.2;
+        
+for (let i = 0; i < linkNav.length; i++) {
+  linkNav[i].addEventListener('click', function(e) { 
+    e.preventDefault();
+
+    let w = window.pageYOffset;
+    let hash = this.href.replace(/[^#]*(.*)/, '$1');  
+    let t = document.querySelector(hash).getBoundingClientRect().top;
+    let start = null;
+
+    requestAnimationFrame(step);
+
+    function step(time) {
+      if (start === null) start = time;
+            
+      let progress = time - start;
+      let r = (t < 0 ? Math.max(w - progress/V, w + t) : Math.min(w + progress/V, w + t));
+      window.scrollTo(0,r);
+                
+      if (r != w + t) {
+        requestAnimationFrame(step)
+      } else {
+        location.hash = hash  
+      }
+    }
+  }, false);
+}
